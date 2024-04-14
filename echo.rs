99e1d3e83d0a92672358c7998f86836cabe3b391
Risto Saarelma <risto.saarelma@iki.fi>
2024-04-14 12:54:05 +0300

    More powerful directory tools

--- echo.rs
+++ echo.rs
@@ -3,16 +3,18 @@
 //! ```cargo
 //! [dependencies]
 //! anyhow = "1"
+//! idm = "0.4"
 //! idm-tools = { path = "/home/rsaarelm/work/idm-tools", version = "*" }
 //! ```
 
 use anyhow::Result;
-use idm_tools::{Outline, read_directory, write_directory};
+use idm_tools::{read_outline_directory, write_outline_directory, Outline};
 
 fn main() -> Result<()> {
     let path = &std::env::args().collect::<Vec<_>>()[1];
-    let outline: Outline = read_directory(path).unwrap();
+    let (outline, style): (Outline, _) = read_outline_directory(path).unwrap();
 
-    //write_directory(path, &outline).unwrap();
+    //print!("{}", idm::to_string_styled(style, &outline)?);
+    write_outline_directory(path, style, &outline).unwrap();
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,15 @@
 use std::{
-    collections::HashMap,
-    fmt,
-    fs::{self, File},
-    io::{self, prelude::*},
+    collections::BTreeMap,
+    fmt::{self, Write},
+    fs,
     path::{Path, PathBuf},
-    str::FromStr,
 };
 
-use anyhow::{bail, Context, Result};
+use anyhow::{bail, Result};
+use idm::ser::Indentation;
 use indexmap::IndexMap;
 use lazy_regex::regex;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
-use walkdir::WalkDir;
 
 pub type Section = ((String,), Outline);
 
@@ -93,165 +91,279 @@ impl Outline {
     }
 }
 
-pub fn read_directory<T: DeserializeOwned>(
+fn read_directory(
+    output: &mut String,
+    style: &mut Option<Indentation>,
+    prefix: &str,
+    outline_mode: bool,
     path: impl AsRef<Path>,
-) -> Result<T> {
-    use std::fmt::Write;
+) -> Result<()> {
+    let mut elts: Vec<(String, PathBuf)> = Vec::new();
 
-    let mut ret = String::new();
-    for e in WalkDir::new(path).into_iter().filter_map(|e| e.ok()) {
-        let depth = e.depth();
-        if depth == 0 {
-            // The root element, do not print out.
+    for e in fs::read_dir(path)? {
+        let e = e?;
+        let path = e.path();
+        let Some(file_name) = path.file_name() else {
+            continue;
+        };
+        let file_name = file_name.to_string_lossy();
+
+        if file_name.starts_with('.') {
+            log::debug!("read_directory: skipping dotfile {path:?}");
             continue;
         }
-        for _ in 1..depth {
-            write!(ret, "  ")?;
+
+        if !is_valid_filename(&file_name) {
+            bail!("read_directory: invalid filename {file_name:?}");
         }
-        let is_dir = e.file_type().is_dir();
-        let file_name = e.file_name().to_string_lossy();
 
-        if is_dir {
-            if !matches!(file_name.parse(), Ok(PathType::Directory)) {
-                bail!(
-                    "subdirectory {:?} has irregular name, invalid collection",
-                    e.file_name()
-                );
+        if path.is_dir() {
+            if outline_mode {
+                elts.push((format!("{file_name}/"), path.into()));
+            } else {
+                elts.push((file_name.into(), path.into()));
             }
-            writeln!(ret, "{file_name}")?;
+        } else if path.is_file() {
+            match path.extension().map(|a| a.to_string_lossy()) {
+                Some(e) if e == "idm" => {
+                    elts.push((
+                        file_name[..file_name.len() - 4].into(),
+                        path.into(),
+                    ));
+                }
+                Some(_) => {
+                    // Push other extensions in as-is.
+                    elts.push((file_name.into(), path.into()));
+                }
+                None => {
+                    // If they don't, output would assume they had .idm
+                    // extensions that got stripped and output with them.
+                    bail!("read_directory: file {file_name:?} must have an extension");
+                }
+            }
         } else {
-            if !matches!(file_name.parse(), Ok(PathType::File)) {
-                bail!(
-                    "file {file_name:?} has irregular name, invalid collection",
-                );
+            // Bail on symlinks.
+            bail!("read_directory: unhandled file type {path:?}");
+        }
+    }
+
+    // Sort into order for outline, make sure names that start with colon come
+    // first.
+    elts.sort_by(|a, b| {
+        (!a.0.starts_with(':'), &a.0).cmp(&(b.0.starts_with(':'), &b.0))
+    });
+
+    for (head, path) in elts {
+        if path.is_dir() {
+            writeln!(output, "{prefix}{head}")?;
+            // Recurse into subdirectory.
+            read_directory(
+                output,
+                style,
+                &format!("{prefix}  "),
+                outline_mode,
+                path,
+            )?;
+        } else if path.is_file() {
+            let text = fs::read_to_string(&path)?;
+
+            // It's a single line, just put it right after the headword.
+            // This is why file names can't have spaces.
+            if !text.contains('\n') {
+                writeln!(output, "{prefix}{head} {}", text.trim())?;
+                continue;
             }
 
-            writeln!(ret, "{file_name}")?;
+            // Multiple lines, need to work with indentations etc.
+            writeln!(output, "{prefix}{head}")?;
+            for line in text.lines() {
+                if line.trim().is_empty() {
+                    writeln!(output)?;
+                    continue;
+                }
+                write!(output, "{prefix}  ")?;
 
-            // Print lines
-            let file = fs::read_to_string(e.path()).unwrap();
+                if line.starts_with(' ') {
+                    match style {
+                        None => *style = Some(Indentation::Spaces(2)),
+                        Some(Indentation::Tabs) => {
+                            bail!("read_directory: inconsistent indentation in {path:?}");
+                        }
+                        _ => {}
+                    }
+                }
+                if line.starts_with('\t') {
+                    match style {
+                        None => *style = Some(Indentation::Tabs),
+                        Some(Indentation::Spaces(_)) => {
+                            bail!("read_directory: inconsistent indentation in {path:?}");
+                        }
+                        _ => {}
+                    }
+                }
 
-            // XXX: Lots of extra parsing work here that gets thrown out, but
-            // we want to know which specific file has errors when the
-            // collection isn't deserializable.
-            if let Err(_e) = idm::from_str::<Outline>(&file) {
-                // XXX: Get IDM errors cleaned up so you can print them here.
-                bail!("file {file_name:?} is not valid IDM");
-            }
-
-            for line in file.lines() {
                 let mut ln = &line[..];
-                let mut depth = depth;
                 // Turn tab indentation into spaces.
                 while ln.starts_with('\t') {
-                    depth += 1;
+                    write!(output, "  ")?;
                     ln = &ln[1..];
                 }
-                for _ in 1..(depth + 1) {
-                    write!(ret, "  ")?;
-                }
-                writeln!(ret, "{ln}")?;
+                writeln!(output, "{ln}")?;
             }
+        } else {
+            // Don't know what this is (symlink?), bail out.
+            bail!("read_directory: invalid path {path:?}");
         }
-    }
-
-    Ok(idm::from_str(&ret)?)
-}
-
-pub fn write_directory<T: Serialize>(
-    path: impl AsRef<Path>,
-    value: &T,
-) -> Result<()> {
-    // Transmute value into an outline.
-    let tree: SimpleOutline = idm::transmute(value)?;
-
-    fn construct(
-        path: &Path,
-        outline: &SimpleOutline,
-        output: &mut HashMap<PathBuf, String>,
-    ) -> anyhow::Result<()> {
-        for ((head,), body) in &outline.0 {
-            let head = head.trim();
-
-            match head.parse::<PathType>() {
-                Ok(PathType::File) => {
-                    // It's a file. Write out the contents.
-                    let path = path.join(head);
-                    output.insert(path, body.to_string());
-                }
-                Ok(PathType::Directory) => {
-                    // Treat it as directory.
-                    let path: PathBuf = path.join(head);
-                    // Recurse into body using the new path.
-                    construct(&path, body, output)?;
-                }
-                _ => {
-                    bail!("invalid directory-level headline: {head:?}")
-                }
-            }
-        }
-
-        Ok(())
-    }
-
-    // Get files from outline.
-    let mut output = HashMap::new();
-    construct(path.as_ref(), &tree, &mut output)?;
-
-    // Delete non-hidden files not in outline.
-    for entry in WalkDir::new(path.as_ref())
-        .into_iter()
-        .filter_map(|e| e.ok())
-        .filter(|e| {
-            e.file_type().is_file()
-                && !e
-                    .file_name()
-                    .to_str()
-                    .map(|s| s.starts_with("."))
-                    .unwrap_or(false)
-        })
-    {
-        if !output.contains_key(entry.path()) {
-            fs::remove_file(entry.path())
-                .with_context(|| "failed to remove file")?;
-        }
-    }
-
-    // Write out outline files.
-    for (path, content) in output {
-        if let Some(dir) = path.parent() {
-            fs::create_dir_all(dir)?;
-        }
-        fs::write(path, content.to_string())?;
     }
 
     Ok(())
 }
 
-/// Classify lines of text into file-like, subdirectory-like or neither (not
-/// parsed).
-enum PathType {
-    File,
-    Directory,
+/// Read heterogeneous IDM outline directory.
+///
+/// Subdirectories are renamed to have a slash behind them so they get
+/// rewritten as subdirectories when the outline is written out.
+pub fn read_outline_directory<T: DeserializeOwned>(
+    path: impl AsRef<Path>,
+) -> Result<(T, Indentation)> {
+    let mut idm = String::new();
+    let mut indentation = None;
+    read_directory(&mut idm, &mut indentation, "", true, path)?;
+
+    Ok((idm::from_str(&idm)?, indentation.unwrap_or_default()))
 }
 
-impl FromStr for PathType {
-    type Err = anyhow::Error;
+/// Read a structured IDM data directory.
+///
+/// Subdirectory names are read as is, subdirectory structure will not be
+/// preserved if the value is written back into directory.
+pub fn read_data_directory<T: DeserializeOwned>(
+    path: impl AsRef<Path>,
+) -> Result<T> {
+    let mut idm = String::new();
+    let mut indentation = None;
+    read_directory(&mut idm, &mut indentation, "", false, path)?;
 
-    fn from_str(s: &str) -> std::prelude::v1::Result<Self, Self::Err> {
-        // Whitelists of acceptable chars.
-        if regex!(r"^[A-Za-z0-9_-]*(\.[A-Za-z0-9_-]+)+$").is_match(s) {
-            Ok(PathType::File)
-        } else if regex!(r"^[A-Za-z0-9_-]+$").is_match(s) {
-            Ok(PathType::Directory)
+    Ok(idm::from_str(&idm)?)
+}
+
+fn build_files(
+    files: &mut BTreeMap<PathBuf, String>,
+    path: impl AsRef<Path>,
+    style: Indentation,
+    data: &Outline,
+) -> Result<()> {
+    // Attribute block
+    for (key, value) in &data.0 .0 {
+        if !is_valid_filename(key) {
+            bail!("build_files: bad attribute name {key:?}");
+        }
+        let path = path.as_ref().join(format!(":{key}.idm"));
+
+        // Ensure value has correct indentation.
+        let value = if value.contains('\n') {
+            // Only do this with values with newlines, since transmuting to
+            // outline will probably mess up the no-trailing-newline semantic
+            // difference.
+            let value: SimpleOutline = idm::transmute(value)?;
+            idm::to_string_styled(style, &value)?
         } else {
-            bail!("Invalid path string")
+            // Newline-less values just get pushed in as is.
+            value.into()
+        };
+
+        files.insert(path, value);
+    }
+
+    // Regular contents
+    for ((headline,), data) in &data.1 {
+        // You get an empty toplevel section when a file ends with an extra
+        // newline. Just ignore these.
+        if headline.trim().is_empty() {
+            continue;
+        }
+
+        let name = if headline.ends_with('/') {
+            &headline[0..headline.len() - 1]
+        } else {
+            &headline[..]
+        };
+        if !is_valid_filename(name) {
+            bail!("build_files: bad headline {headline:?}");
+        }
+
+        if headline.ends_with('/') {
+            // Create a subdirectory.
+            build_files(files, &path.as_ref().join(headline), style, data)?;
+            continue;
+        }
+
+        let file_name = if headline.contains('.') {
+            headline.into()
+        } else {
+            format!("{headline}.idm")
+        };
+
+        let path = path.as_ref().join(file_name);
+        files.insert(path, idm::to_string_styled(style, data)?);
+    }
+    Ok(())
+}
+
+fn write_directory(
+    path: impl AsRef<Path>,
+    style: Indentation,
+    data: &Outline,
+) -> Result<()> {
+    // See that we can build all the contents successfully before deleting
+    // anything.
+    let mut files = BTreeMap::default();
+    build_files(&mut files, path.as_ref(), style, data)?;
+
+    // Clean out old stuff.
+    for e in fs::read_dir(path.as_ref())? {
+        let e = e?;
+        let path = e.path();
+
+        let Some(file_name) = path.file_name() else {
+            continue;
+        };
+        let file_name = file_name.to_string_lossy();
+
+        if file_name.starts_with('.') {
+            log::debug!("write_directory: skipping dotfile {path:?}");
+            continue;
+        }
+
+        if path.is_dir() {
+            log::debug!("write_directory: Removing existing dir {path:?}");
+            fs::remove_dir_all(path)?;
+        } else {
+            log::debug!("write_directory: Removing existing file {path:?}");
+            fs::remove_file(path)?;
         }
     }
+
+    for (path, content) in files {
+        if let Some(dir) = path.parent() {
+            fs::create_dir_all(dir)?;
+        }
+        fs::write(path, content)?;
+    }
+
+    Ok(())
 }
 
-/// Something looks like a filename if it has a period followed immediately by
-/// a non-whitespace character.
-fn looks_like_filename(name: impl AsRef<str>) -> bool {
-    regex!(r"\.\S").is_match(name.as_ref())
+pub fn write_outline_directory<T: Serialize>(
+    path: impl AsRef<Path>,
+    style: Indentation,
+    value: &T,
+) -> Result<()> {
+    let tree: Outline = idm::transmute(value)?;
+
+    write_directory(path, style, &tree)
+}
+
+fn is_valid_filename(s: impl AsRef<str>) -> bool {
+    regex!(r"^:?[A-Za-z0-9_-][.A-Za-z0-9_-]*$").is_match(s.as_ref())
 }
